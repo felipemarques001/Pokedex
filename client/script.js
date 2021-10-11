e56c81635af6f052eae11fe1b471dd556c7cdc55
Felipe <felipemarquesgg@outlook.com>
2021-10-11 20:33:07 -0300

    thirdCommit

--- client/script.js
+++ client/script.js
@@ -15,23 +15,52 @@ fetchPokemon()
 function showPokemon(data, num){
     let ul = document.getElementById('ul');
     if(num < 10){
-        ul.innerHTML += `<li class="${data[1]}">
-        <section class="description">
-            <h2 class="name">${data[0]}</h2>
-            <p id="${data[1]}">#0${num}</p>
-            <p id="${data[1]}">${data[1]}</p>
+        ul.innerHTML += `<li class="${data[1]}" onclick="showPokemonDescription(this)">
+        <section>
+            <section class="description">
+                <h2 class="name">${data[0]}</h2>
+                <p id="${data[1]}">#0${num}</p>
+                <p id="${data[1]}">${data[1]}</p>
+            </section>
+            <section class="status">
+                <h4>Abilities</h4>
+                <p>1°: ${data[2]}</p>
+                <h4>Status</h4>
+                <p>HP: ${data[3]}</p>
+                <p>Attack: ${data[4]}</p>
+                <p>Defense: ${data[5]}</p>
+                <p>Speed: ${data[6]}</p>
+            </section>
         </section>
+        
         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${num}.svg" alt="">
     </li>` 
     }else{
-        ul.innerHTML += `<li class="${data[1]}">
-                        <section class="description">
-                            <h2 class="name">${data[0]}</h2>
-                            <p id="${data[1]}">#${num}</p>
-                            <p id="${data[1]}">${data[1]}</p>
-                        </section>
-                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${num}.svg" alt="${data[0]}">
-                    </li>`
+        ul.innerHTML += `<li class="${data[1]}" onclick="showPokemonDescription(this)">
+        <section>
+            <section class="description">
+                <h2 class="name">${data[0]}</h2>
+                <p id="${data[1]}">#${num}</p>
+                <p id="${data[1]}">${data[1]}</p>
+            </section>
+            <section class="status">
+                <h4>Abilities</h4>
+                <p>1°: ${data[2]}</p>
+                <h4>Status</h4>
+                <p>HP: ${data[3]}</p>
+                <p>Attack: ${data[4]}</p>
+                <p>Defense: ${data[5]}</p>
+                <p>Speed: ${data[6]}</p>
+            </section>
+        </section>
+        
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${num}.svg" alt="">
+    </li>`  
     }
-    
 }
+
+function showPokemonDescription(th){
+    th.classList.toggle('show');
+}
+
+
--- server.js
+++ server.js
@@ -9,7 +9,15 @@ app.use(cors())
 app.get('/pokemon/:id', async (req, res) =>{
     try {
         const {data} = await axios(`https://pokeapi.co/api/v2/pokemon/${req.params.id}`);
-        let pokemon= [data.name, data.types[0].type.name];
+        let pokemon= [
+            data.name,
+            data.types[0].type.name, 
+            data.abilities[0].ability.name, 
+            data.stats[0].base_stat,
+            data.stats[1].base_stat,
+            data.stats[2].base_stat,
+            data.stats[5].base_stat
+        ];
         return res.json(pokemon)
     } catch (error) {
         console.error(error)
